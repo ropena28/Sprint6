7f417c919f463d3a8fe972dd3af63a22042b667b
Juancito <cjuan.chona@hotmail.com>
2022-08-01 00:11:51 -0300

    problematica 3 completa

--- Problematica3.sql
+++ Problematica3.sql
@@ -18,7 +18,9 @@ order by (SELECT branch_name FROM sucursal)
 
 
 -- Seleccionar los préstamos que tengan fecha en abril, junio y agosto, ordenándolos por importe (necesito ayuda porque se me complico)
-
+SELECT substr(loan_date,6,2) as mes,* FROM prestamo
+WHERE mes='04' or mes='06' or mes='08'
+ORDER BY loan_total ASC;
 
 
 
@@ -35,3 +37,30 @@ LIMIT 5;
 SELECT count(customer_DNI)
 FROM cliente
 WHERE dob < '1972';
+
+-- Seleccionar de la tabla de préstamos, 
+-- los préstamos con un importe mayor a $80.000 y
+--  los préstamos prendarios utilizando la unión 
+-- de tablas/consultas (recordar que en las bases
+-- de datos la moneda se guarda como integer, en 
+-- este caso con 2 centavos)
+
+SELECT loan_total*1.0/100 as loan,loan_type from prestamo 
+WHERE loan >80000 
+UNION
+SELECT loan_total*1.0/100 as loan,loan_type FROM prestamo WHERE loan_type = 'PRENDARIO'
+ORDER BY loan ASC
+;
+
+-- Seleccionar los prestamos cuyo importe sea mayor que el importe medio de todos los prestamos
+-- SELECT avg(loan_total*1.0/100) FROM prestamo; importe medio?
+
+SELECT loan_total*1.0/100 as loan,* FROM prestamo
+WHERE loan > (SELECT avg(loan_total*1.0/100) FROM prestamo)
+ORDER BY loan ASC
+;
+
+-- Obtener el importe total de los prestamos agrupados por tipo de préstamos
+SELECT sum(loan_total)*1.0/100 as loan_total_accu,loan_type FROM prestamo
+GROUP by loan_type
+;
